da9763d446f9c8f6fd3ea1db410c876150ae298d
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-03-16 13:22:17 +0200

    Use of API

--- src/modules/util.js
+++ src/modules/util.js
@@ -1,16 +1,48 @@
-import { nameInput, renderScores, scoreInput } from './DOM.js';
+import {
+  nameInput,
+  postErrorContainer,
+  renderScores,
+  scoreInput,
+  throwGetError,
+  throwPostError,
+} from './DOM.js';
 
-export const scores = [];
+const gameUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0b3IROaa6TNhBfSLZFnF/scores';
 
-export const submitScore = () => {
-  scores.push({
-    name: nameInput.value,
-    score: scoreInput.value,
-  });
-  nameInput.value = '';
-  scoreInput.value = '';
+export const submitScore = async () => {
+  try {
+    postErrorContainer.innerHTML = '';
+    const response = await fetch(gameUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: nameInput.value,
+        score: scoreInput.value,
+      }),
+    });
+    if (!response.ok) throw new Error(`Enable to post your score (status: ${response.status})`);
+    nameInput.value = '';
+    scoreInput.value = '';
+  } catch (err) {
+    throwPostError(err.message);
+  }
 };
 
-export const refrechScoretable = () => {
-  renderScores(scores);
+export const refrechScoretable = async () => {
+  try {
+    const response = await fetch(gameUrl);
+    if (!response.ok) {
+      throw new Error(
+        `Enable to access your score table (status: ${response.status})`,
+      );
+    }
+    const data = await response.json();
+    const scores = data.result;
+    if (!scores.length) throw new Error('The score table is empty');
+    renderScores(scores);
+  } catch (err) {
+    throwGetError(err.message);
+  }
 };
